db816e28be9e6179ce8f2150c5f2d69c3134f44d
Venkat Gorla <venkatagorla@gmail.com>
2025-04-02 18:13:51 +0530

    Migrate utility function for investment calculation to ts - pending some cleanup and unit tests

--- src/util/investment.ts
+++ src/util/investment.ts
@@ -4,23 +4,61 @@
 // - annualInvestment: The amount invested every year
 // - expectedReturn: The expected (annual) rate of return
 // - duration: The investment duration (time frame)
+// export function calculateInvestmentResults({
+//   initialInvestment,
+//   annualInvestment,
+//   expectedReturn,
+//   duration,
+// }) {
+//   const annualData = [];
+//   let investmentValue = initialInvestment;
+
+//   for (let i = 0; i < duration; i++) {
+//     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+//     investmentValue += interestEarnedInYear + annualInvestment;
+//     annualData.push({
+//       year: i + 1, // year identifier
+//       interest: interestEarnedInYear, // the amount of interest earned in this year
+//       valueEndOfYear: investmentValue, // investment value at end of year
+//       annualInvestment: annualInvestment, // investment added in this year
+//     });
+//   }
+
+//   return annualData;
+// }
+
+export type InvestmentParams = {
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
+};
+
+export type InvestmentYearData = {
+  year: number;
+  interest: number;
+  valueEndOfYear: number;
+  annualInvestment: number;
+};
+
 export function calculateInvestmentResults({
   initialInvestment,
   annualInvestment,
   expectedReturn,
   duration,
-}) {
-  const annualData = [];
+}: InvestmentParams): InvestmentYearData[] {
+  const annualData: InvestmentYearData[] = [];
   let investmentValue = initialInvestment;
 
   for (let i = 0; i < duration; i++) {
     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
     investmentValue += interestEarnedInYear + annualInvestment;
+
     annualData.push({
-      year: i + 1, // year identifier
-      interest: interestEarnedInYear, // the amount of interest earned in this year
-      valueEndOfYear: investmentValue, // investment value at end of year
-      annualInvestment: annualInvestment, // investment added in this year
+      year: i + 1,
+      interest: interestEarnedInYear,
+      valueEndOfYear: investmentValue,
+      annualInvestment,
     });
   }
 
