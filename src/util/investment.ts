7ce07e5c06929f2bb990f847bce465d417fd95b0
Venkat Gorla <venkatagorla@gmail.com>
2025-04-02 18:30:32 +0530

    [Investment utility] remove commented code - ready for unit testing

--- src/util/investment.ts
+++ src/util/investment.ts
@@ -1,32 +1,3 @@
-// This function expects a JS object as an argument
-// The object should contain the following properties
-// - initialInvestment: The initial investment amount
-// - annualInvestment: The amount invested every year
-// - expectedReturn: The expected (annual) rate of return
-// - duration: The investment duration (time frame)
-// export function calculateInvestmentResults({
-//   initialInvestment,
-//   annualInvestment,
-//   expectedReturn,
-//   duration,
-// }) {
-//   const annualData = [];
-//   let investmentValue = initialInvestment;
-
-//   for (let i = 0; i < duration; i++) {
-//     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-//     investmentValue += interestEarnedInYear + annualInvestment;
-//     annualData.push({
-//       year: i + 1, // year identifier
-//       interest: interestEarnedInYear, // the amount of interest earned in this year
-//       valueEndOfYear: investmentValue, // investment value at end of year
-//       annualInvestment: annualInvestment, // investment added in this year
-//     });
-//   }
-
-//   return annualData;
-// }
-
 export type InvestmentParams = {
   initialInvestment: number;
   annualInvestment: number;
@@ -58,7 +29,7 @@ export function calculateInvestmentResults({
       year: i + 1,
       interest: interestEarnedInYear,
       valueEndOfYear: investmentValue,
-      annualInvestment,
+      annualInvestment, // investment added in this year
     });
   }
 
